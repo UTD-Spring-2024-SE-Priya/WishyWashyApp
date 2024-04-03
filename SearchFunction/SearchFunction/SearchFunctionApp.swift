d1a06a6583c7e792cc3458ad6b608115cdd09314
WajihAnwar <98074747+WajihAnwar@users.noreply.github.com>
2024-04-03 17:28:42 -0500

    Add files via upload

---
+++ SearchFunction/SearchFunction/SearchFunctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchFunctionApp.swift
+//  SearchFunction
+//
+//  Created by Wajih Anwar on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchFunctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SearchRestaurants()
+        }
+    }
+}
